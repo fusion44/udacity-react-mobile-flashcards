0d6969b4e7db9154334e3198d5c92abfda820faa
Stefan <fusion44@laptop-antergos.fritz.box>
2017-11-11 11:22:35 +0100

    feat: add navigation

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from "react"
 import { StyleSheet, Text, View, StatusBar, FlatList } from "react-native"
-import { getDecks } from "./_helpers"
-import DeckItem from "./components/DeckItem"
+import Home from "./components/Home"
+import DeckDetail from "./components/DeckDetail"
 import { Constants } from "expo"
+
+import { StackNavigator } from "react-navigation"
+
+const ViewStack = StackNavigator({
+  Home: {
+    screen: Home
+  },
+  DeckDetail: {
+    screen: DeckDetail
+  }
+})
 
 const AppStatusBar = ({ backgroundColor, ...props }) => {
   return (
@@ -13,47 +24,11 @@ const AppStatusBar = ({ backgroundColor, ...props }) => {
 }
 
 export default class App extends React.Component {
-  state = {
-    loading: true,
-    decks: []
-  }
-
-  componentWillMount() {
-    getDecks()
-      .then(jsondecks => {
-        let decks = []
-        Object.entries(JSON.parse(jsondecks)).forEach(([key, deck]) =>
-          decks.push(deck)
-        )
-
-        this.setState(...this.state, {
-          decks,
-          loading: false
-        })
-      })
-      .catch(error => console.log(error))
-  }
-
-  onItemClick(title) {}
-
-  renderDeckItem = deck => {
-    return (
-      <DeckItem key={deck.title} deck={deck} onItemClick={this.onItemClick} />
-    )
-  }
-
   render() {
-    const { decks } = this.state
-
     return (
       <View style={styles.container}>
         <AppStatusBar backgroundColor="blue" barStyle="light-content" />
-        <FlatList
-          data={decks}
-          extraData={this.state}
-          keyExtractor={(deck, index) => deck.title}
-          renderItem={this.renderDeckItem}
-        />
+        <ViewStack />
       </View>
     )
   }
---
+++ components/Home.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { FlatList } from "react-native"
+import { getDecks } from "../_helpers"
+import DeckItem from "./DeckItem"
+import { Constants } from "expo"
+
+export default class Home extends React.Component {
+  state = {
+    loading: true,
+    decks: []
+  }
+
+  componentWillMount() {
+    getDecks()
+      .then(jsondecks => {
+        let decks = []
+        Object.entries(JSON.parse(jsondecks)).forEach(([key, deck]) =>
+          decks.push(deck)
+        )
+
+        this.setState(...this.state, {
+          decks,
+          loading: false
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  onItemClick(title) {
+    this.props.navigation.navigate("DeckDetail", { title })
+  }
+
+  renderDeckItem = deck => {
+    return (
+      <DeckItem
+        key={deck.title}
+        deck={deck}
+        onItemClick={this.onItemClick.bind(this)}
+      />
+    )
+  }
+
+  render() {
+    const { decks } = this.state
+
+    return (
+      <FlatList
+        data={decks}
+        extraData={this.state}
+        keyExtractor={(deck, index) => deck.title}
+        renderItem={this.renderDeckItem}
+      />
+    )
+  }
+}
